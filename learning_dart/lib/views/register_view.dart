d979ef95cc857254c4f15b59a717389f83d5129e
collins iregi <collinsregs@gmail.com>
2023-02-05 18:17:50 +0300

    firts commit

---
+++ learning_dart/lib/views/register_view.dart
@@ -0,0 +1,81 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:learning_dart/firebase_options.dart';
+
+class RegisteView extends StatefulWidget {
+  const RegisteView({super.key});
+
+  @override
+  State<RegisteView> createState() => _RegisteViewState();
+}
+
+class _RegisteViewState extends State<RegisteView> {
+  late final TextEditingController _email;
+  late final TextEditingController _password;
+  @override
+  void initState() {
+    _email = TextEditingController();
+    _password = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _email.dispose();
+    _password.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Register')),
+      body: Column(
+        children: [
+          TextField(
+            controller: _email,
+            keyboardType: TextInputType.emailAddress,
+            decoration:
+                const InputDecoration(hintText: 'enter your email here'),
+          ),
+          TextField(
+            controller: _password,
+            obscureText: true,
+            enableSuggestions: false,
+            autocorrect: false,
+            decoration:
+                const InputDecoration(hintText: 'enter your password here'),
+          ),
+          TextButton(
+            onPressed: () async {
+              final email = _email.text;
+              final password = _password.text;
+              try {
+                final userCredentials = await FirebaseAuth.instance
+                    .createUserWithEmailAndPassword(
+                        email: email, password: password);
+                print(userCredentials);
+              } on FirebaseAuthException catch (e) {
+                if (e.code == 'weak-password') {
+                  print('weak password');
+                } else if (e.code == 'email-already-in-use') {
+                  print('email is already in use');
+                } else if (e.code == 'invalid-email') {
+                  print('invalid email entered');
+                }
+              }
+            },
+            child: const Text('register'),
+          ),
+          TextButton(
+              onPressed: () {
+                Navigator.of(context)
+                    .pushNamedAndRemoveUntil("/login/", (route) => false);
+              },
+              child: Text('Already registerd? Login hear'))
+        ],
+      ),
+    );
+  }
+}
