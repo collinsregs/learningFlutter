cd55b073123c26fe9be3cd8e5a0146385113aad6
collins iregi <collinsregs@gmail.com>
2023-02-06 17:29:40 +0300

    step 8

---
+++ learning_dart/lib/services/auth/auth_provider.dart
@@ -0,0 +1,16 @@
+import 'package:learning_dart/services/auth/auth_user.dart';
+
+//creating an abstract class
+abstract class AuthProvider {
+  AuthUser? get currentUser;
+  Future<AuthUser> logIn({
+    required String email,
+    required String password,
+  });
+  Future<AuthUser> createUser({
+    required String email,
+    required String password,
+  });
+  Future<void> logOut();
+  Future<void> sendEmailVerification();
+}
